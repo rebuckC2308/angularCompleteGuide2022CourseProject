420607a589be086562c91244da0b3cf4bda57586
Chris Rebuck <rebuckc2308@gmail.com>
2022-04-29 17:45:34 -0500

    created recipe model.  can display multiple test recipes on webpage

--- courseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ courseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'Test Description',
+      'https://imagesvc.meredithcorp.io/v3/jumpstartpure/image?url=https://static.onecms.io/wp-content/uploads/sites/43/2022/03/08/7016-french-toast-mfs-010.jpg&w=640&h=360&q=90&c=cc'
+    ),
+    new Recipe(
+      'Test Recipe',
+      'Test Description',
+      'https://imagesvc.meredithcorp.io/v3/jumpstartpure/image?url=https://static.onecms.io/wp-content/uploads/sites/43/2022/03/08/7016-french-toast-mfs-010.jpg&w=640&h=360&q=90&c=cc'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
