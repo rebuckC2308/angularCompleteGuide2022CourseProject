5d63dc9dfae0d475c2fcf034d98cce5f2eef0333
Chris Rebuck <rebuckc2308@gmail.com>
2022-05-04 10:41:53 -0500

    can now display individual recipe data when you click/select a specific recipe

--- courseProject/src/app/app.module.ts
+++ courseProject/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { RecipesComponent } from './recipes/recipes.component';
     ShoppingListComponent,
     ShoppingEditComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
